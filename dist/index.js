f356c543ce695eaf11a4bf29be4f9ec13efac282
Young Sun <42809902+YoungSunBurst@users.noreply.github.com>
2023-01-01 23:01:02 +0900

    feat: histogram graph

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,14 @@
 const defaultConfig = {
+    backgroundColor: "white",
     barColor: "",
+    padding: 15,
+    scaleFunction: (frame) => {
+        return {
+            scale: frame / 60,
+            done: frame >= 60,
+        };
+    },
+    gridColor: "black",
 };
 export class MultiHistogram {
     constructor(table, config) {
@@ -10,43 +19,114 @@ export class MultiHistogram {
     setValue(table) {
         this.tables_ = table;
     }
-    draw(ctx, width, height, scale) {
-        ctx.canvas.width = width * scale;
-        ctx.canvas.height = height * scale;
-        let s = 0.1;
-        const reDraw = () => {
+    draw(ctx, width, height, pixelRatio) {
+        var _a, _b, _c, _d, _e, _f, _g, _h, _j;
+        const canvasWidth = width * pixelRatio;
+        const canvasHeight = height * pixelRatio;
+        ctx.canvas.width = canvasWidth;
+        ctx.canvas.height = canvasHeight;
+        ctx.fillStyle = (_a = this.config_.backgroundColor) !== null && _a !== void 0 ? _a : defaultConfig.backgroundColor;
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        let frame = 1;
+        const padding = (_b = this.config_.padding) !== null && _b !== void 0 ? _b : defaultConfig.padding;
+        const xStart = ((_d = (_c = this.config_.yGrid) === null || _c === void 0 ? void 0 : _c.boundSize) !== null && _d !== void 0 ? _d : 0) + padding;
+        const yStart = padding;
+        const graphHeight = canvasHeight - ((_f = (_e = this.config_.xGrid) === null || _e === void 0 ? void 0 : _e.boundSize) !== null && _f !== void 0 ? _f : 0) - padding * 2;
+        const graphWidth = canvasWidth - xStart - padding;
+        const scaleFunc = (_g = this.config_.scaleFunction) !== null && _g !== void 0 ? _g : defaultConfig.scaleFunction;
+        if (this.config_.xGrid) {
             ctx.save();
-            ctx.scale(scale, scale);
+            ctx.translate(xStart, yStart + graphHeight);
+            const gridValueSize = (this.config_.xAxis.max - this.config_.xAxis.min) / (this.config_.xGrid.gridCount - 1);
+            const values = Array(this.config_.xGrid.gridCount).fill(1).map((v, i) => this.config_.xAxis.min + i * gridValueSize);
+            this.drawGrid(ctx, this.config_.xGrid, graphWidth, (_h = this.config_.gridColor) !== null && _h !== void 0 ? _h : defaultConfig.gridColor, values, 0 /* TextDirection.X */);
+            ctx.restore();
+        }
+        if (this.config_.yGrid) {
+            ctx.save();
+            ctx.transform(0, -1, -1, 0, xStart, yStart + graphHeight);
+            const gridValueSize = (this.config_.yAxis.max - this.config_.yAxis.min) / (this.config_.yGrid.gridCount - 1);
+            const values = Array(this.config_.yGrid.gridCount).fill(1).map((v, i) => this.config_.yAxis.min + i * gridValueSize);
+            this.drawGrid(ctx, this.config_.yGrid, graphHeight, (_j = this.config_.gridColor) !== null && _j !== void 0 ? _j : defaultConfig.gridColor, values, 2 /* TextDirection.YRotate */);
+            ctx.restore();
+        }
+        const drawWithTransition = () => {
+            var _a;
+            ctx.save();
+            ctx.fillStyle = (_a = this.config_.backgroundColor) !== null && _a !== void 0 ? _a : defaultConfig.backgroundColor;
+            ctx.fillRect(xStart, yStart, graphWidth, graphHeight);
+            ctx.translate(xStart, yStart);
+            const { scale, done } = scaleFunc(frame++);
             const xCount = (this.config_.xAxis.max - this.config_.xAxis.min + 1) / this.config_.xInterval;
-            const xBarWidth = width / xCount;
-            const yScale = height / (this.config_.yAxis.max - this.config_.yAxis.min + 1) * s;
-            console.log(this.tables_);
+            const xBarWidth = graphWidth / xCount;
             this.tables_.forEach(table => {
-                this.drawTable(table, ctx, xBarWidth, height, yScale);
+                var _a, _b, _c, _d;
+                const yMin = (_b = (_a = table.yAxis) === null || _a === void 0 ? void 0 : _a.min) !== null && _b !== void 0 ? _b : this.config_.yAxis.min;
+                const yMax = (_d = (_c = table.yAxis) === null || _c === void 0 ? void 0 : _c.max) !== null && _d !== void 0 ? _d : this.config_.yAxis.max;
+                const yScale = graphHeight / (yMax - yMin + 1) * scale;
+                this.drawTable(table, ctx, xBarWidth, graphHeight, yMin, yScale);
             });
             ctx.restore();
-            if (s < 1) {
+            if (!done) {
                 requestAnimationFrame(() => {
-                    s += 0.01;
-                    reDraw();
+                    drawWithTransition();
                 });
             }
         };
-        reDraw();
+        drawWithTransition();
     }
-    drawTable(table, ctx, xBarWidth, height, yScale) {
+    drawTable(table, ctx, xBarWidth, height, yMin, yScale) {
         var _a, _b;
         ctx.fillStyle = (_a = table.fillColor) !== null && _a !== void 0 ? _a : defaultConfig.barColor;
         ctx.strokeStyle = (_b = table.strokeColor) !== null && _b !== void 0 ? _b : defaultConfig.barColor;
         for (let x = this.config_.xAxis.min, i = 0; x < this.config_.xAxis.max; x += this.config_.xInterval, i++) {
             if (table.bars[x]) {
                 const pos = i * xBarWidth;
-                const h = ((table.bars[x] - this.config_.yAxis.min) * yScale);
+                const h = ((table.bars[x] - yMin) * yScale);
                 ctx.fillRect(pos, height - h, xBarWidth, h);
                 ctx.strokeRect(pos, height - h, xBarWidth, h);
-                console.log(ctx.fillStyle, pos, height - h, xBarWidth, h, table.bars[x], this.config_.yAxis.min);
             }
         }
-        console.log("draw");
+    }
+    drawGrid(ctx, gridConfig, width, color, values, textDirection) {
+        var _a, _b;
+        const gridSize = width / (gridConfig.gridCount - 1);
+        const smallGridSize = width / (gridConfig.smallGridCount - 1);
+        ctx.save();
+        const fontSize = (_a = this.config_.fontSize) !== null && _a !== void 0 ? _a : 10;
+        ctx.font = `${fontSize}px ${(_b = this.config_.fontFamily) !== null && _b !== void 0 ? _b : "sans-serif"}`;
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+        ctx.lineWidth = 2;
+        ctx.moveTo(0, 10);
+        ctx.lineTo(width, 10);
+        let pos = 0;
+        for (let i = 0; i < gridConfig.gridCount; i++) {
+            ctx.moveTo(pos, 10);
+            ctx.lineTo(pos, 20);
+            const textMetrics = ctx.measureText(values[i].toString());
+            ctx.save();
+            if (textDirection === 0 /* TextDirection.X */) {
+                ctx.transform(1, 0, 0, 1, pos - textMetrics.width / 2, 20 + fontSize);
+            }
+            else if (textDirection === 1 /* TextDirection.Y */) {
+                ctx.transform(-1, 0, 0, 1, pos + textMetrics.width / 2, 20 + fontSize);
+            }
+            else if (textDirection === 2 /* TextDirection.YRotate */) {
+                ctx.transform(0, -1, -1, 0, pos - fontSize / 3, 25 + textMetrics.width);
+            }
+            ctx.fillText(values[i].toString(), 0, 0);
+            ctx.restore();
+            pos += gridSize;
+        }
+        pos = 0;
+        for (let i = 0; i < gridConfig.smallGridCount; i++) {
+            ctx.moveTo(pos, 10);
+            ctx.lineTo(pos, 15);
+            pos += smallGridSize;
+        }
+        ctx.stroke();
+        ctx.restore();
     }
 }
--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,15 @@
-export type Bar = {[key: number]: number};
+export type Bar = { [key: number]: number };
 
 export interface Table {
     strokeColor?: string;
     fillColor?: string;
     bars: Bar;
+    yAxis?: Ranges;
 }
 
 export interface Grid {
     //
     boundSize: number;
-    fontFamily: string;
-    fontSize: number;
     gridCount: number;
     smallGridCount: number;
     // displayValues: number[];
@@ -27,60 +26,149 @@ export interface Config {
     yAxis: Ranges;
     xGrid?: Grid;
     yGrid?: Grid;
+    gridColor?: string;
+    scaleFunction?: (frame: number) => { scale: number, done: boolean };
+    fontFamily?: string;
+    fontSize?: number;
+    padding?: number;
+    backgroundColor?: string;
 }
 
 const defaultConfig = {
+    backgroundColor: "white",
     barColor: "",
+    padding: 15,
+    scaleFunction: (frame: number) => {
+        return {
+            scale: frame / 60,
+            done: frame >= 60,
+        };
+    },
+    gridColor: "black",
 };
+
+const enum TextDirection {
+    X,
+    Y,
+    YRotate
+}
 
 export class MultiHistogram {
     private tables_: Table[] = [];
     private config_: Config;
+
     constructor(table: Table[], config: Config) {
         this.tables_ = table;
         this.config_ = config;
     }
+
     public setValue(table: Table[]): void {
         this.tables_ = table;
     }
 
-    public draw(ctx: CanvasRenderingContext2D, width: number, height: number, scale: number) {
-        ctx.canvas.width = width * scale;
-        ctx.canvas.height = height * scale;
-        let s = 0.1;
-        const reDraw = () => {
+    public draw(ctx: CanvasRenderingContext2D, width: number, height: number, pixelRatio: number) {
+        const canvasWidth = width * pixelRatio;
+        const canvasHeight = height * pixelRatio;
+        ctx.canvas.width = canvasWidth;
+        ctx.canvas.height = canvasHeight;
+        ctx.fillStyle = this.config_.backgroundColor ?? defaultConfig.backgroundColor;
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        let frame = 1;
+        const padding = this.config_.padding ?? defaultConfig.padding;
+        const xStart = (this.config_.yGrid?.boundSize ?? 0) + padding;
+        const yStart = padding;
+        const graphHeight = canvasHeight - (this.config_.xGrid?.boundSize ?? 0) - padding * 2;
+        const graphWidth = canvasWidth - xStart - padding;
+        const scaleFunc = this.config_.scaleFunction ?? defaultConfig.scaleFunction;
+        if (this.config_.xGrid) {
             ctx.save();
-            ctx.scale(scale, scale);
+            ctx.translate(xStart, yStart + graphHeight);
+            const gridValueSize = (this.config_.xAxis.max - this.config_.xAxis.min) / (this.config_.xGrid.gridCount - 1);
+            const values = Array(this.config_.xGrid.gridCount).fill(1).map((v, i) => this.config_.xAxis.min + i * gridValueSize);
+            this.drawGrid(ctx, this.config_.xGrid, graphWidth, this.config_.gridColor ?? defaultConfig.gridColor, values, TextDirection.X);
+            ctx.restore();
+        }
+        if (this.config_.yGrid) {
+            ctx.save();
+            ctx.transform(0, -1, -1, 0, xStart, yStart + graphHeight);
+            const gridValueSize = (this.config_.yAxis.max - this.config_.yAxis.min) / (this.config_.yGrid.gridCount - 1);
+            const values = Array(this.config_.yGrid.gridCount).fill(1).map((v, i) => this.config_.yAxis.min + i * gridValueSize);
+            this.drawGrid(ctx, this.config_.yGrid, graphHeight, this.config_.gridColor ?? defaultConfig.gridColor, values, TextDirection.YRotate);
+            ctx.restore();
+        }
+        const drawWithTransition = () => {
+            ctx.save();
+            ctx.fillStyle = this.config_.backgroundColor ?? defaultConfig.backgroundColor;
+            ctx.fillRect(xStart, yStart, graphWidth, graphHeight);
+            ctx.translate(xStart, yStart);
+            const {scale, done} = scaleFunc(frame++);
             const xCount = (this.config_.xAxis.max - this.config_.xAxis.min + 1) / this.config_.xInterval;
-            const xBarWidth = width / xCount;
-            const yScale = height / (this.config_.yAxis.max - this.config_.yAxis.min + 1) * s;
-            console.log(this.tables_);
+            const xBarWidth = graphWidth / xCount;
             this.tables_.forEach(table => {
-                this.drawTable(table, ctx, xBarWidth, height, yScale);
+                const yMin = table.yAxis?.min ?? this.config_.yAxis.min;
+                const yMax = table.yAxis?.max ?? this.config_.yAxis.max;
+                const yScale = graphHeight / (yMax - yMin + 1) * scale;
+                this.drawTable(table, ctx, xBarWidth, graphHeight, yMin, yScale);
             });
             ctx.restore();
-            if (s < 1) {
+            if (!done) {
                 requestAnimationFrame(() => {
-                    s += 0.01;
-                    reDraw();
+                    drawWithTransition();
                 });
             }
         };
-        reDraw();
+        drawWithTransition();
     }
 
-    private drawTable(table: Table, ctx: CanvasRenderingContext2D, xBarWidth: number, height: number, yScale: number) {
+    private drawTable(table: Table, ctx: CanvasRenderingContext2D, xBarWidth: number, height: number, yMin: number, yScale: number) {
         ctx.fillStyle = table.fillColor ?? defaultConfig.barColor;
         ctx.strokeStyle = table.strokeColor ?? defaultConfig.barColor;
         for (let x = this.config_.xAxis.min, i = 0; x < this.config_.xAxis.max; x += this.config_.xInterval, i++) {
             if (table.bars[x]) {
                 const pos = i * xBarWidth;
-                const h = ((table.bars[x] - this.config_.yAxis.min) * yScale);
+                const h = ((table.bars[x] - yMin) * yScale);
                 ctx.fillRect(pos, height - h, xBarWidth, h);
                 ctx.strokeRect(pos, height - h, xBarWidth, h);
-                console.log(ctx.fillStyle, pos, height - h, xBarWidth, h, table.bars[x], this.config_.yAxis.min);
             }
         }
-        console.log("draw");
+    }
+
+    private drawGrid(ctx: CanvasRenderingContext2D, gridConfig: Grid, width: number, color: string, values: number[], textDirection: TextDirection) {
+        const gridSize = width / (gridConfig.gridCount - 1);
+        const smallGridSize = width / (gridConfig.smallGridCount - 1);
+        ctx.save();
+        const fontSize = this.config_.fontSize ?? 10;
+        ctx.font = `${fontSize}px ${this.config_.fontFamily ?? "sans-serif"}`;
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+        ctx.lineWidth = 2;
+        ctx.moveTo(0, 10);
+        ctx.lineTo(width, 10);
+        let pos = 0;
+        for (let i = 0; i < gridConfig.gridCount; i++) {
+            ctx.moveTo(pos, 10);
+            ctx.lineTo(pos, 20);
+            const textMetrics = ctx.measureText(values[i].toString());
+            ctx.save();
+            if (textDirection === TextDirection.X) {
+                ctx.transform(1, 0, 0, 1, pos - textMetrics.width / 2, 20 + fontSize);
+            } else if (textDirection === TextDirection.Y) {
+                ctx.transform(-1, 0, 0, 1, pos + textMetrics.width / 2, 20 + fontSize);
+            } else if (textDirection === TextDirection.YRotate) {
+                ctx.transform(0, -1, -1, 0, pos - fontSize / 3, 25 + textMetrics.width);
+            }
+            ctx.fillText(values[i].toString(), 0, 0);
+            ctx.restore();
+            pos += gridSize;
+        }
+        pos = 0;
+        for (let i = 0; i < gridConfig.smallGridCount; i++) {
+            ctx.moveTo(pos, 10);
+            ctx.lineTo(pos, 15);
+            pos += smallGridSize;
+        }
+        ctx.stroke();
+        ctx.restore();
     }
 }
